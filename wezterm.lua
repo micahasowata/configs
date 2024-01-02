406bf03634269f8e8818782f0a08b049ac24b9b9
Micah Asowata <andrewgary963@gmail.com>
2024-01-02 16:10:06 +0000

    feat: configure tab bar
    set the styles and colors and effects of the tab in different states

--- wezterm.lua
+++ wezterm.lua
@@ -8,4 +8,44 @@ end
 
 config.color_scheme = "zenbones_dark"
 
+config.show_tab_index_in_tab_bar = false
+
+config.colors = {
+	tab_bar = {
+		background = "#0A0A0A",
+
+		active_tab = {
+			bg_color = "#0A0A0A",
+			fg_color = "#ffffff",
+			intensity = "Bold",
+			underline = "Double",
+			italic = true,
+		},
+
+		inactive_tab = {
+			bg_color = "#252525",
+			fg_color = "#f5f5f5",
+			intensity = "Normal",
+			italic = false,
+		},
+
+		inactive_tab_hover = {
+			bg_color = "#252525",
+			fg_color = "#f5f5f5",
+			intensity = "Bold",
+			strikethrough = true,
+		},
+
+		new_tab = {
+			bg_color = "#252525",
+			fg_color = "#f5f5f5",
+		},
+
+		new_tab_hover = {
+			bg_color = "#f5f5f5",
+			fg_color = "#252525",
+		},
+	},
+}
+
 return config
